24c22ad5d31010079de36e6a7c9475a823755a72
minhho <minh.ho89@gmail.com>
2022-02-23 16:37:56 +0700

    1st commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test_app/screens/json_handle.dart';
+import 'package:test_app/screens/http_handle.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: JsonHandleScreen(),
+      home: const HttpHandle(),
     );
   }
 }
--- lib/screens/http_handle.dart
+++ lib/screens/http_handle.dart
@@ -1,26 +1,63 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
-class HttpHandle extends StatelessWidget {
+class HttpHandle extends StatefulWidget {
   const HttpHandle({Key? key}) : super(key: key);
+
+  @override
+  State<HttpHandle> createState() => _HttpHandleState();
+}
+
+class _HttpHandleState extends State<HttpHandle> {
+  List posts = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          child: TextButton(
-            child: Text('HTTP GET'),
-            onPressed: _httpGetTest,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                FutureBuilder(
+                    future:
+                        _getUrl('https://jsonplaceholder.typicode.com/posts'),
+                    builder: (ctx, snapshot) {
+                      if (snapshot.connectionState != ConnectionState.done) {
+                        return Center(child: CircularProgressIndicator());
+                      }
+                      if (snapshot.hasError) {
+                        return Text('Oops, error: ${snapshot.error}');
+                      }
+                      // if (!snapshot.hasData) {
+                      //   return Text('There is nothing to show');
+                      // }
+
+                      print(posts.length);
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: posts.length,
+                        itemBuilder: (ctx, i) => ListTile(
+                          title: Text(posts[i]['title']),
+                        ),
+                      );
+                    })
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  Future<void> _httpGetTest() async {
-    var url = Uri.parse('https://us.com/people/1234');
-    Response response = await get(url);
-    print(response.statusCode);
+  Future<void> _getUrl(String url) async {
+    var parsedUrl = Uri.parse(url);
+    Response response = await get(parsedUrl);
+    posts = [...jsonDecode(response.body)];
+    posts.forEach((element) {
+      print(element['title']);
+    });
   }
 }
